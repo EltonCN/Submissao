ac17ae730ffc1c4e4bad054c745980c2701220ac
EltonCN <43186596+EltonCN@users.noreply.github.com>
2022-09-28 20:25:37 -0300

    Feature: Câmera e controles básicos
    - Implementação da câmera em 1ª pessoa
    - Implementação da movimentação e pulo

---
+++ Shadow/Assets/Scripts/Movement.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] float horizontalVelocity;
+    [SerializeField] float jumpForce = 1f;
+    [SerializeField] Transform forwardReference;
+    [SerializeField] Rigidbody rigidBody;
+
+    bool jumping = false;
+    bool cancelada = false;
+
+
+    Vector2 input;
+    
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        input = Vector2.zero;
+    }
+
+    public void ReceiveMovementInput(InputAction.CallbackContext context)
+    {
+        input = context.ReadValue<Vector2>();
+        input *= horizontalVelocity;
+
+        
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 velocity = ( forwardReference.right * input.x + forwardReference.forward * input.y);
+
+        velocity.y = rigidBody.velocity.y;
+
+        if(jumping)
+        {
+            rigidBody.AddForce(new Vector3(0,jumpForce,0), ForceMode.Impulse);
+            jumping = false;
+        }
+
+        rigidBody.velocity = velocity;
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {        
+        if(context.phase == InputActionPhase.Canceled)
+        {
+            cancelada = true;
+        }
+
+        if(context.phase == InputActionPhase.Performed && Mathf.Abs(rigidBody.velocity.y) <= 0.1f  && cancelada)
+        {
+            jumping = true;
+            cancelada = false;
+        }
+        else
+        {
+            jumping = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
