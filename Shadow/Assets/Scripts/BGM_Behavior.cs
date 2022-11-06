db5a714e0c631da4a6bc853b1646431b82df9f38
Vinicius-Yglesias <114174659+Vinicius-Yglesias@users.noreply.github.com>
2022-11-06 19:21:43 -0300

    Programação_música
    Programação do comportamento da música para tocar apenas de tempos em tempos ao invés de loop direto. Por enquanto ta entre 30 e 90 segundos, mas isso pode ser debatido.

---
+++ Shadow/Assets/Scripts/BGM_Behavior.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGM_Behavior : MonoBehaviour
+{
+    public AudioSource bgm;
+    public AudioClip loop1;
+    public float timer;
+    public float total;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Bgm_b());
+    }
+
+    IEnumerator Bgm_b()
+    {
+        PlayBGM();
+
+        timer = Random.Range(30f, 90f);
+        total = bgm.clip.length + timer;
+        
+        yield return new WaitForSeconds(bgm.clip.length + timer);
+
+        StartCoroutine(Bgm_b());
+    }
+
+    void PlayBGM()
+    {
+        bgm.clip = loop1;
+        bgm.Play();
+    }
+}
