7bf3f08c947c75427b8b760a3d82cd66d381980f
Vinicius-Yglesias <114174659+Vinicius-Yglesias@users.noreply.github.com>
2022-11-23 14:45:05 -0300

    atualização_música
    música com volume maior

--- Shadow/Assets/Scripts/Audio/UI_Sounds.cs
+++ Shadow/Assets/Scripts/Audio/UI_Sounds.cs
@@ -10,7 +10,7 @@ public class UI_Sounds : MonoBehaviour, IPointerEnterHandler
     public AudioSource UI;
     public AudioClip UI_clip;
 
-    public void OnPointerEnter(PointerEventData ped)
+    public void OnPointerEnter(PointerEventData button_sound)
     {
         UI.clip = UI_clip;
         UI.pitch = Random.Range(0.99f, 1.01f);
