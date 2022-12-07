b1056dae82bbcd84e1ba09f64c44928b4b1df9e4
EltonCN <43186596+EltonCN@users.noreply.github.com>
2022-12-07 14:11:05 -0300

    Apresentação

--- Shadow/Assets/Scripts/CloseExitDoor.cs
+++ Shadow/Assets/Scripts/CloseExitDoor.cs
@@ -1,16 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class CloseExitDoor : MonoBehaviour {
     [SerializeField] GameObject player;
     [SerializeField] GameObject exitDoor;
+    [SerializeField] UnityEvent onDoorClose;
     private bool doorClosed = false;
 
     void OnCollisionEnter( Collision collider ) {
         if ( !doorClosed && GameObject.ReferenceEquals(collider.gameObject, player) ) {
             exitDoor.GetComponent<MeshCollider>().enabled = true;
             doorClosed = true;
+            onDoorClose.Invoke();
         }
     }
 }
